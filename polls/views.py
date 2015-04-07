04284e24f7d226b7ac19ccdc2df10179e0a8ea28
Chris Odegard <c.odegard@gmail.com>
2015-04-06 22:50:39 -0400

    continued part 3 of the django tutorial
    stopped at heading “Removing hardcoded URLs in templates”

--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from Django.shortcuts import render
 from django.template import RequestContext, loader
 
-from polls.models import Question
+from .models import Question
 
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #the above object is called directly in polls/index
     template = loader.get_template('polls/index.html')
     context = RequestContext(request, {
         'latest_question_list': latest_question_list,
@@ -16,7 +18,12 @@ def index(request):
     #url request to polls/index (right??)(yes! basically.)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    #same as above: question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
     
 def results(request, question_id):
     response = "You're looking at the results of question %s."
